01b2d71f4b0f0d30491b6bf6e48d374dc11a80d9
Kevin Kobus <109695538+kevinkobus@users.noreply.github.com>
2023-03-25 11:45:45 -0600

    added delete and tweaked edit button to functionality (#1)

--- memelist-generator/components/Meme.jsx
+++ memelist-generator/components/Meme.jsx
@@ -1,15 +1,19 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios"
+import axios from "axios";
 
+// function to set state for memes that display in preview
 function Meme() {
   const [meme, setMeme] = useState({
     topText: "",
     bottomText: "",
     randomImage: "http://i.imgflip.com/1bij.jpg",
   });
+
+  // setting state for memes when received from api
   const [allMemes, setAllMemes] = useState([]);
-  const [listMemes, setListMemes] = useState([])
-  
+  const [listMemes, setListMemes] = useState([]);
+  const [isEditing, setIsEditing] = useState(false)
+
   // useEffect function with axios get request to pull top 100 meme images from api
   useEffect(() => {
     axios
@@ -19,6 +23,7 @@ function Meme() {
       .catch((error) => console.log(error));
   }, []);
 
+  // function to pick a random meme from array of memes in state
   function getMemeImage() {
     const randomNumber = Math.floor(Math.random() * allMemes.length);
     const url = allMemes[randomNumber].url;
@@ -28,6 +33,7 @@ function Meme() {
     }));
   }
 
+  // function to handle changes to input fields
   function handleChange(event) {
     const { name, value } = event.target;
     setMeme((prevMeme) => ({
@@ -36,19 +42,30 @@ function Meme() {
     }));
   }
 
+  // function to handle edit to memes in list
   function handleEdit(event) {
-    const memeIndex = event.target.dataset.index
-    const meme = listMemes[memeIndex]
-    setMeme(meme)
-    setListMemes((prevMemes) =>{
-      
-      return listMemes.filter((value, idx) => {
-        console.log(idx != memeIndex)
-        return idx != memeIndex
-      })
-    })
+    setIsEditing(!isEditing)
+    // console.log(event.target.dataset.index) 
+    // const memeIndex = event.target.dataset.index;
+    // const meme = listMemes[memeIndex];
+    // setMeme(meme);
+    // setListMemes((prevMemes) => {
+    //   return listMemes.filter((value, idx) => {
+    //     console.log(idx != memeIndex);
+    //     return idx != memeIndex;
+    //   });
+    // });
   }
+function handleDelete(index) {
+  // console.log(index)
+  setListMemes(listMemes.filter((meme, pos)=> {
+    return index !== pos
+  }))
+}
 
+// pos = index position
+
+  // function to save meme to list below preview
   function savedList(event) {
     event.preventDefault();
     setListMemes((prevListMemes) => {
@@ -60,18 +77,24 @@ function Meme() {
       randomImage: "http://i.imgflip.com/1bij.jpg",
     });
   }
-  console.log(listMemes)
+  // console.log(listMemes);
 
+  // mapping through memes saved in state to display them to the list with edit/delete
   const displayMemes = listMemes.map((meme, index) => (
- <div className="meme-list" key={index}>
-        <img src={meme.randomImage} className="meme-list-image" />
-        <h2 className="meme--text top">{meme.topText}</h2>
-        <h2 className="meme--text bottom">{meme.bottomText}</h2>  
-        <button className="edit-button" data-index={index} onClick={handleEdit} >Edit</button>
-      </div>))
+    <div className="meme-list" key={index}>
+      <img src={meme.randomImage} className="meme-list-image" />
+      { isEditing ? <input placeholder="top text"/> : <h2 className="meme--text top">{meme.topText}</h2>}
+      { isEditing ? <input placeholder="btm text" /> : <h2 className="meme--text bottom">{meme.bottomText}</h2>}
+      <button className="edit-button" data-index={index} onClick={handleEdit}>
+        {isEditing ? "Save" : "Edit"}
+      </button>
+      <button className="delete-btn" onClick={() => handleDelete(index)}>
+        Delete
+      </button>)
+    </div>
+  ));
 
-
-
+  // rendering of elements to the DOM
   return (
     <main>
       <div className="form">
@@ -94,17 +117,18 @@ function Meme() {
         <button className="form--button" onClick={getMemeImage}>
           Get a new meme image 🖼
         </button>
-        <button className="save-button" onClick={savedList}>Save meme</button>
+        <button className="save-button" onClick={savedList}>
+          Save meme
+        </button>
       </div>
       <div className="meme">
         <img src={meme.randomImage} className="meme--image" />
         <h2 className="meme--text top">{meme.topText}</h2>
-        <h2 className="meme--text bottom">{meme.bottomText}</h2>  
+        <h2 className="meme--text bottom">{meme.bottomText}</h2>
       </div>
       <div className="memes-container">{displayMemes}</div>
-    
     </main>
   );
 }
 
-export default Meme
+export default Meme;
