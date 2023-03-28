9b3d0d9a1c4c9cef08ca9e761e6669ab33552ed7
Ashley McKellar <ashley.l.mckellar@gmail.com>
2023-03-27 20:41:26 -0700

    added notes

--- memelist-generator/components/Meme.jsx
+++ memelist-generator/components/Meme.jsx
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios"
 
+//sets state with default random image and empty top and bottom text, this is what appears in the preview section
+
 function Meme() {
   const [meme, setMeme] = useState({
     topText: "",
     bottomText: "",
     randomImage: "http://i.imgflip.com/1bij.jpg",
   });
-  const [allMemes, setAllMemes] = useState([]);
-  const [listMemes, setListMemes] = useState([])
+  const [allMemes, setAllMemes] = useState([]); //the memes received from the api
+  const [listMemes, setListMemes] = useState([]) //the created memes array displayed below the preview page
   
   // useEffect function with axios get request to pull top 100 meme images from api
   useEffect(() => {
@@ -19,6 +21,10 @@ function Meme() {
       .catch((error) => console.log(error));
   }, []);
 
+  //function to pick a random meme from the array of memes saved in state 
+  //saves url of random image, sets new meme state variable with old fields, but random image url is updated to the one
+  //chosen with the random function
+
   function getMemeImage() {
     const randomNumber = Math.floor(Math.random() * allMemes.length);
     const url = allMemes[randomNumber].url;
@@ -28,27 +34,32 @@ function Meme() {
     }));
   }
 
+
+  //handles change to input fields, renders text to preview image while it's typed in
   function handleChange(event) {
-    const { name, value } = event.target;
+    const { name, value } = event.target; //contains the values for name and value, is called every time a new letter is typed
     setMeme((prevMeme) => ({
       ...prevMeme,
       [name]: value,
     }));
   }
-
+  //handles edit to meme in list when edit button is clicked, pushes the target meme to top (preview) div and jumps 
+  //up to the top of the page so you don't have to scroll all the way up.
   function handleEdit(event) {
-    const memeIndex = event.target.dataset.index
-    const meme = listMemes[memeIndex]
+    const memeIndex = event.target.dataset.index //data-index in jsx is contained in dataset.index here
+    
     window.scrollTo(0,0)
-    setMeme(meme)
-    setListMemes((prevMemes) =>{
+    setMeme(listMemes[memeIndex] ) //updates state to the meme whose edit button you clicked, moving it up to the top
+    setListMemes(() =>{ //updates state for listMemes
       
       return listMemes.filter((value, idx) => {
-        console.log(idx != memeIndex)
+        // console.log(idx != memeIndex)
         return idx != memeIndex
       })
     })
   }
+  //filters memes saved in the listMemes state, filters out the meme whose delete button was clicked, so that it's no 
+  //longer rendered in the list
 
   function handleDelete(index) {
     // console.log(index)
@@ -57,6 +68,8 @@ function Meme() {
     }))
   }
 
+  //function to save meme to list below preview, adds the meme to the list memes array saved in state, then empties
+  //the input boxes and back to the default photo
   function savedList(event) {
     event.preventDefault();
     setListMemes((prevListMemes) => {
@@ -70,6 +83,11 @@ function Meme() {
   }
   console.log(listMemes)
 
+  //
+
+
+  //maps over the listMemes array and renders the created memes, along with an edit and delete button
+
   const displayMemes = listMemes.map((meme, index) => (
  <div className="meme-list" key={index}>
         <img src={meme.randomImage} className="meme-list-image" />
@@ -77,6 +95,8 @@ function Meme() {
         <h2 className="meme--text bottom">{meme.bottomText}</h2>  
         <div id="buttons">
           <button className="edit-button" data-index={index} onClick={handleEdit} >Edit</button>
+          {/* handleEdit is called with the event parameter, whereas handleDelete is called by an anonymous function with 
+          the index parameter */}
           <button className="delete-btn" onClick={() => handleDelete(index)}>Delete</button>
         </div>
       </div>))
